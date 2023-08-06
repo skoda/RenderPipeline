e8696967618e14ad1bcb6cd958c8e9f8c666377d
Chuck Skoda <charles.skoda@gmail.com>
2023-08-05 22:39:48 -0700

    Light and texture stuff.

---
+++ src/texture.ts
@@ -0,0 +1,61 @@
+import Vector from './vector'
+
+export class TextureCoord extends Vector {
+  get u() {
+    return this[0]
+  }
+  set u(u: number) {
+    this[0] = u
+  }
+
+  get v() {
+    return this[1]
+  }
+  set v(v: number) {
+    this[1] = v
+  }
+
+  static withUV(u: number, v: number) {
+    return new TextureCoord([u, v])
+  }
+
+  private constructor(values: number[]) {
+    super(values)
+  }
+
+  clone() {
+    return new TextureCoord(this)
+  }
+}
+
+export class Texture {
+  data: ImageData
+
+  static cache: Record<string, Texture>
+
+  static async withURL(url: string) {
+    if (Texture.cache[url]) return Texture.cache[url]
+
+    return new Promise((resolve, reject) => {
+      const img = new Image()
+      img.onload = () => {
+        const { width, height } = img
+        const canvas = document.createElement('canvas')
+        canvas.width = width
+        canvas.height = height
+
+        const context = canvas.getContext('2d')
+
+        if (!context) return reject("Couldn't get canvas context for loading texture.")
+
+        context.drawImage(img, 0, 0)
+        const data = context.getImageData(0, 0, width, height)
+        return resolve(new Texture(data))
+      }
+    })
+  }
+
+  private constructor(data: ImageData) {
+    this.data = data
+  }
+}
--- src/utility.ts
+++ src/utility.ts
@@ -1,5 +1,1 @@
 export const clamp = (n: number, max: number, min = 0) => Math.min(Math.max(n, min), max)
-
-export type KeyOfType<T, V> = keyof {
-  [P in keyof T as T[P] extends V? P: never]: any
-}
